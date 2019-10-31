e7bb09bd509f95a0ad1c4327226130524dc284dc
Will Drougas <wdrougas@gmail.com>
2019-10-31 14:45:01 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
-
+class CashRegister
+  
+  attr_accessor :total, :discount, :price, :title, :quantity
+  
+  def initialize(total = 0)
+    @total = total
+  end
+  
+  def discount
+    @discount = 20
+  end
+  
+  def add_item(title, price, quantity=1)
+    self.total += price * quantity
+  end
+  
+  def apply_discount()
+    self.total += self.discount
+ end
+ 
+end
